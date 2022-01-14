674e6e38bf7b087a22d5f2db1cc816bed0faf850
AlexeyKonstantinov78 <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2022-01-14 09:49:47 +0300

    fix slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const sliderAll = () => {
-    const swiper1 = new Swiper('.swiper1');  
+    const swiper1 = new Swiper('.swiper1', {
+        slidesPerView: 1,
+    });  
     
-    const swiper2 = new Swiper('.swiper2');  
+    const swiper2 = new Swiper('.swiper2', {
+        slidesPerView: 1,
+    });  
 };
 
 const pets = () => {
@@ -19,19 +23,27 @@ const pets = () => {
     });  
 };
 
-window.addEventListener('DOMContentLoaded', () => {
+const reload = () => {
+    const initSlider1 = document.querySelector('.swiper1'),
+        initSlider2 = document.querySelector('.swiper2'),
+        initSlider3 = document.querySelector('.swiper3');
+    
+    if (initSlider3.classList.contains('swiper-initialized') && window.screen.width > 840) location.reload();
+    if (initSlider2.classList.contains('swiper-initialized') && window.screen.width > 640) location.reload();
+    if (initSlider1.classList.contains('swiper-initialized') && window.screen.width > 640) location.reload();
+};
 
-    // console.log('DOMContentLoaded' + ' : ' + window.screen.width);
+window.addEventListener('DOMContentLoaded', () => {
+    
     if (window.screen.width <= 640) sliderAll();
-    if (window.screen.height <= 840) pets();
+    if (window.screen.width <= 840) pets();
 });
 
 window.addEventListener('resize', () => {
-
-    // console.log('resize' + ' : ' + window.screen.width);
+    
     if (window.screen.width <= 640) sliderAll();
-    if (window.screen.height <= 840) pets();
+    if (window.screen.width <= 840) pets();
+    if (window.screen.width > 640) reload();
 
 });
 
-// const swiper1 = new Swiper('.swiper1');
